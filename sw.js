22c6462ed88ae7eab24e2ab22fb763cc19a46e60
jajangam <macnoly@gmail.com>
2026-06-06 07:56:18 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'rabi-v5';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k){ return k !== CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
